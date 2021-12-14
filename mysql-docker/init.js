f0198d34c829de721321306f0dd0dbfcfc55d1ee
pleroma1112 <pleroma1112@naver.com>
2021-12-14 17:08:02 +0900

    myslq-init update

--- mysql-docker/init.js
+++ mysql-docker/init.js
@@ -1,23 +1,29 @@
 const hasher = require('pbkdf2-password')()
 const fs = require('fs')
 
-hasher({passsword:'04010401'},(err,pass,salt,hash)=>{
-    const sql = `
-        use web;
-        create table user(
-            id varchar(20),
-            _hash varchar(1000),
-            salt varchar(1000),
-            primary key(id)
-        );
-        insert into user(id,_hash,salt) values(
-            'web-admin',
-            '${hash}',
-            '${salt}'
-        );
-    `
-    fs.writeFile('init.sql',sql,'utf8',(err)=>{
-        if(err) throw err
-        console.log('Done!')
+hasher({password:'04010401'},(err,pass,salt,hash)=>{
+    const hash1 = hash
+    hasher({password : '04010401',salt},(err,pass,salt,hash)=>{
+        // console.log(hash)
+        if(hash1===hash){
+            const sql = `
+                use web;
+                create table user(
+                    id varchar(20),
+                    _hash varchar(1000),
+                    salt varchar(1000),
+                    primary key(id)
+                );
+                insert into user(id,_hash,salt) values(
+                    'web-admin',
+                    '${hash}',
+                    '${salt}'
+                );
+            `
+            fs.writeFile('init.sql',sql,'utf8',(err)=>{
+                if(err) throw err
+                console.log('Done!')
+            })
+        }
     })
 })
--- mysql-docker/init.sql
+++ mysql-docker/init.sql
@@ -1,14 +1,14 @@
 
-        use web;
-        create table user(
-            id varchar(20),
-            _hash varchar(1000),
-            salt varchar(1000),
-            primary key(id)
-        );
-        insert into user(id,_hash,salt) values(
-            'web-admin',
-            'J0Ktudg2lYK1NV38iAxCDB5FhxjEOsgxBx5lGCZdd6GqZrf0jcVWkzJZ3a9gcjZbWs7TXJyF/3OKp/v5rTqcpHNkHitZ1oYnO/dOIwtd5zOHuPdvhBcqPvVFaEzXWWccmZZCEcl54nIap/FQqYNS1Kk8HEjLzM7y3eIa2x6Rxq0=',
-            'LDpI5nKb20aeTvdRBRH6nDLmO3HRetzgnTbE6nyLkNgPAgNvPrbDPLYMSkTPuuD6eXB/E44+E8F7Q+t6fhMDxQ=='
-        );
-    
+                use web;
+                create table user(
+                    id varchar(20),
+                    _hash varchar(1000),
+                    salt varchar(1000),
+                    primary key(id)
+                );
+                insert into user(id,_hash,salt) values(
+                    'web-admin',
+                    'g5q1KXrYnU4UmijkZCftssdWl0T7boRW3IC+kTuonBbcOCBhCvG4oR93pnQfYzEnLxn5WBPgpJBdNx0d2oBztFeWJEtcxI81Y9o7hk2zgWe/avvqealbPYUjokgwU3k+GHlPr+roKfQgtOgWQDlOAtho+RFmZqFxcu5fz/oMqzQ=',
+                    '3wjOoWU/4/3mrCzh4PcLP6D7n6mA1sq6u9fjewwmAfHBXYZvrNzeNLcy9MQBjQ3B3Tfe7Z1TCAQjZbVVcv5dKA=='
+                );
+            
