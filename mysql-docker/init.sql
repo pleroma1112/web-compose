2bbc8a48b5d9a909e77dda2a1e8780b37b9f9021
pleroma1112 <pleroma1112@naver.com>
2021-12-14 16:28:00 +0900

    dockerfile, update

--- mysql-docker/init.sql
+++ mysql-docker/init.sql
@@ -10,5 +10,5 @@
             'web-admin',
             'J0Ktudg2lYK1NV38iAxCDB5FhxjEOsgxBx5lGCZdd6GqZrf0jcVWkzJZ3a9gcjZbWs7TXJyF/3OKp/v5rTqcpHNkHitZ1oYnO/dOIwtd5zOHuPdvhBcqPvVFaEzXWWccmZZCEcl54nIap/FQqYNS1Kk8HEjLzM7y3eIa2x6Rxq0=',
             'LDpI5nKb20aeTvdRBRH6nDLmO3HRetzgnTbE6nyLkNgPAgNvPrbDPLYMSkTPuuD6eXB/E44+E8F7Q+t6fhMDxQ=='
-        )
+        );
     
